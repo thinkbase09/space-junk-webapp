418db86bd4fada656be81f2d2ff01776f5577767
thinkbase09 <thinkbase1229@gmail.com>
2025-06-02 06:35:35 +0900

    지금이니

--- tle-backend/app.py
+++ tle-backend/app.py
@@ -4,8 +4,17 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from skyfield.api import load, wgs84
 
+
+
 app = Flask(__name__)
 CORS(app)
+
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Render가 제공하는 포트 사용
+    app.run(host="0.0.0.0", port=port)
+
 
 @app.route('/api/debris')
 def get_debris():
